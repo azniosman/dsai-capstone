9ea46c65b7e560e370d2945c7ceaf2d7d09d8479
azniosman <az@azni.me>
2026-02-11 01:45:48 +0800

    fix: add Secrets Manager VPC endpoint for ECS task secret retrieval
    ECS tasks in the private subnet couldn't reach AWS Secrets Manager
    to pull DB credentials and JWT secret during startup, causing
    ResourceInitializationError. Added secretsmanager interface endpoint.

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -172,6 +172,19 @@ resource "aws_vpc_endpoint" "logs" {
   }
 }
 
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private.id]
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-secretsmanager-endpoint"
+  }
+}
+
 ###############################################################################
 # Security Group - ALB
 ###############################################################################
